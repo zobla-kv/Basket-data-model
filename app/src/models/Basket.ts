a76f7649328b1479100e439086fb8dc3a06012a2
zobla <zobla.kv@gmail.com>
2025-09-26 00:18:02 +0200

    move add and price methods logic from Basket to Database

--- app/src/models/Basket.ts
+++ app/src/models/Basket.ts
@@ -3,8 +3,6 @@ import database from './Database';
 
 class Basket<T extends Product> {
   private _products: T[];
-  private _chargeRules = database.chargeRules;
-  private _specialOffer = database.specialOffer;
 
   constructor(products: T[]) {
     this._products = products;
@@ -14,46 +12,12 @@ class Basket<T extends Product> {
     return this._products;
   }
 
-  add(product: T): T['code'] {
-    this._products.push(product);
-    return product.code;
+  add(code: T['code']): T['code'] {
+    return database.addToBasket(code);
   }
 
   price(): string {
-    let price = 0;
-
-    this._products.forEach((product) => {
-      const productPrice = this._getNumericPrice(product.price);
-      price += productPrice;
-    });
-
-    price = price - this._getShippingCost(price);
-
-    price = price - this._specialOffer.getDiscount(this._products);
-
-    return `$${price.toFixed(2)}`;
-  }
-
-  // transform '$0' -> 0
-  private _getNumericPrice(price: string): number {
-    return parseFloat(price.replace('$', ''));
-  }
-
-  private _getShippingCost(total: number): number {
-    const parsedRules = Object.entries(this._chargeRules)
-      .map(([key, value]) => ({
-        threshold: parseInt(key.replace('$', '')),
-        cost: parseFloat(value.replace('$', '')),
-      }))
-      .sort((a, b) => a.threshold - b.threshold);
-
-    for (const rule of parsedRules) {
-      if (total < rule.threshold) {
-        return rule.cost;
-      }
-    }
-
-    return parsedRules[parsedRules.length - 1].cost;
+    return database.getBasketTotalPrice();
   }
 }
 
--- app/src/models/Database.ts
+++ app/src/models/Database.ts
@@ -2,12 +2,13 @@ import { Product } from './Product';
 import Widget from './Widget';
 
 interface IDatabase {
-  widgets: {
-    items: Widget[];
-    add: (widget: Product) => Product['code'];
-  };
+  widgets: Widget[];
+  basketItems: Product['code'][];
   chargeRules: {
-    [threshold: string]: string;
+    rules: {
+      [threshold: string]: string;
+    };
+    getDiscount: (total: number) => number;
   };
   specialOffer: {
     text: string;
@@ -16,54 +17,47 @@ interface IDatabase {
 }
 
 class Database implements IDatabase {
-  private readonly _widgets: IDatabase['widgets'] = {
-    items: [
-      {
-        code: 'R01',
-        name: 'Red Widget',
-        price: '$32.95',
-      },
-      {
-        code: 'G01',
-        name: 'Green Widget',
-        price: '$24.95',
-      },
-      {
-        code: 'B01',
-        name: 'Blue Widget',
-        price: '$7.95',
-      },
-    ],
-    add: (widget: Product) => this._handleAddWidget(widget),
-  };
+  private _widgets: IDatabase['widgets'] = [
+    {
+      code: 'R01',
+      name: 'Red Widget',
+      price: '$32.95',
+    },
+    {
+      code: 'G01',
+      name: 'Green Widget',
+      price: '$24.95',
+    },
+    {
+      code: 'B01',
+      name: 'Blue Widget',
+      price: '$7.95',
+    },
+  ];
+
+  private _basketItems: IDatabase['basketItems'] = ['R01', 'G01', 'B01'];
 
   private _chargeRules: IDatabase['chargeRules'] = {
-    $50: '$4.95',
-    $89: '$2.95',
-    $90: '$0',
+    rules: {
+      $50: '$4.95',
+      $89: '$2.95',
+      $90: '$0',
+    },
+    getDiscount: (total: number) => this._getChargeRulesDiscount(total),
   };
 
   private _specialOffer: IDatabase['specialOffer'] = {
     text: 'buy one red widget, get the second half price',
-    getDiscount: (products: Product[]) => {
-      const redWidget = this._widgets.items.find((w) => w.code === 'R01');
-      if (!redWidget) {
-        return 0;
-      }
-
-      const redCount = products.filter((p) => p.code === 'R01').length;
-      if (redCount < 2) {
-        return 0;
-      }
-
-      const redPrice = parseFloat(redWidget.price.replace('$', ''));
-
-      return Math.floor(redPrice / 2);
-    },
+    getDiscount: (products: Product[]) =>
+      this._getSpecialOfferDiscount(products),
   };
 
   get widgets() {
     return this._widgets;
+  }
+
+  get basketItems() {
+    return this._basketItems;
   }
 
   get chargeRules() {
@@ -74,9 +68,74 @@ class Database implements IDatabase {
     return this._specialOffer;
   }
 
-  private _handleAddWidget(widget: Widget): Widget['code'] {
-    this.widgets.items.push(widget);
-    return widget.code;
+  addToBasket(code: Product['code']): Product['code'] {
+    this._basketItems.push(code);
+    return code;
+  }
+
+  getBasketTotalPrice(): string {
+    let price = 0;
+    const products: Product[] = [];
+
+    this._basketItems.forEach((code) => {
+      const product = this._getProductByCode(code);
+
+      if (!product) {
+        return;
+      }
+
+      const productPrice = this._getNumericPrice(product.price);
+      price += productPrice;
+      products.push(product);
+    });
+
+    price = price - this._chargeRules.getDiscount(price);
+
+    price = price - this._specialOffer.getDiscount(products);
+
+    return `$${price.toFixed(2)}`;
+  }
+
+  private _getProductByCode(code: Product['code']): Product | undefined {
+    return this._widgets.find((p) => p.code === code);
+  }
+
+  // transform '$0' -> 0
+  private _getNumericPrice(price: string): number {
+    return parseFloat(price.replace('$', ''));
+  }
+
+  private _getChargeRulesDiscount(total: number): number {
+    const parsedRules = Object.entries(this._chargeRules.rules)
+      .map(([key, value]) => ({
+        threshold: parseInt(key.replace('$', '')),
+        cost: parseFloat(value.replace('$', '')),
+      }))
+      .sort((a, b) => a.threshold - b.threshold);
+
+    for (const rule of parsedRules) {
+      if (total < rule.threshold) {
+        return rule.cost;
+      }
+    }
+
+    return parsedRules[parsedRules.length - 1].cost;
+  }
+
+  private _getSpecialOfferDiscount(products: Product[]): number {
+    const redWidget = products.find((w) => w.code === 'R01');
+    if (!redWidget) {
+      return 0;
+    }
+
+    const redCount = products.filter((p) => p.code === 'R01').length;
+    if (redCount < 2) {
+      return 0;
+    }
+
+    const redPrice = this._getNumericPrice(redWidget.price);
+
+    return Math.floor(redPrice / 2);
   }
 }
 
