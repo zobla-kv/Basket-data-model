85166a2273c9ff2c24ac27d6acee21da2c0354ec
zobla <zobla.kv@gmail.com>
2025-09-25 20:28:55 +0200

    update widgets data logic in database

--- app/src/models/Database.ts
+++ app/src/models/Database.ts
@@ -1,25 +1,43 @@
-import type Widget from './Widget';
+import { Product } from './Product';
+import Widget from './Widget';
 
-const widgets: Widget[] = [
-  {
-    code: 'R01',
-    name: 'Red Widget',
-    price: '$32.95',
-  },
-  {
-    code: 'G01',
-    name: 'Green Widget',
-    price: '$24.95',
-  },
-  {
-    code: 'B01',
-    name: 'Blue Widget',
-    price: '$7.95',
-  },
-];
+interface IDatabase {
+  widgets: {
+    items: Widget[];
+    add: (widget: Product) => Product['code'];
+  };
+}
 
-class Database {
-  public readonly items: readonly unknown[] = widgets;
+class Database implements IDatabase {
+  private readonly _widgets: IDatabase['widgets'] = {
+    items: [
+      {
+        code: 'R01',
+        name: 'Red Widget',
+        price: '$32.95',
+      },
+      {
+        code: 'G01',
+        name: 'Green Widget',
+        price: '$24.95',
+      },
+      {
+        code: 'B01',
+        name: 'Blue Widget',
+        price: '$7.95',
+      },
+    ],
+    add: (widget: Product) => this._handleAddWidget(widget),
+  };
+
+  get widgets() {
+    return this._widgets;
+  }
+
+  private _handleAddWidget(widget: Widget): Widget['code'] {
+    this.widgets.items.push(widget);
+    return widget.code;
+  }
 }
 
 export default new Database();
